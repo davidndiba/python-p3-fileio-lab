ffc14283f8948175fca9b65386ace51279b02d8e
David <david.ndiba@student.moringaschool.com>
2023-08-30 12:23:28 +0300

    fileio lab

--- lib/testing/file_io_test.py
+++ lib/testing/file_io_test.py
@@ -1,31 +1,34 @@
+import tempfile
 from file_io import write_file, append_file, read_file
+import uuid
+import os
 
-def test_write_file(tmp_path):
-    """Test write_file()"""
-    file_name = tmp_path / "test_file"
-    file_content = "This is a test content."
-    write_file(file_name, file_content)
-    with open(f'{file_name}.txt', 'r') as f:
-        file_content_read = f.read()
-    assert file_content_read == file_content
+'''Function write_files() in file_io.py'''
+def test_write_file():
 
-def test_append_file(tmp_path):
-    """Test append_file()"""
+  #Create temp directory to write file to and see if it exits.
+  with tempfile.TemporaryDirectory() as tmp_dirname:
+    filename = str(uuid.uuid4())
+    expected_file_content = str(uuid.uuid4())
+    write_file(f'{tmp_dirname}/{filename}',expected_file_content)
+    assert os.path.exists(f'{tmp_dirname}/{filename}.txt') == True
 
-    file_name = tmp_path / "test_file"
-    file_content = "This is a test content."
-    append_content = "\nAppended content."
-    write_file(file_name, file_content)
-    append_file(file_name, append_content)
-    with open(f'{file_name}.txt', 'r') as f:
-        file_content_read = f.read()
-    assert file_content_read == file_content + append_content
+    # Check file content
+    file_content = ''
+    with open(f'{tmp_dirname}/{filename}.txt') as f:
+      file_content = f.read()
 
-def test_read_file(tmp_path):
-    """Test read_file()"""
+      assert file_content == expected_file_content
+def test_write_file():
+    '''file_io.py: write_file() takes a filename and input and writes the input to a file.'''
+    write_file('lib/testing/write.txt', 'Hello, World!')
+    f = open('lib/testing/write.txt', 'r')
+    assert(f.read() == 'Hello, World!')
+    os.remove('lib/testing/write.txt')
 
-    file_name = tmp_path / "test_file"
-    file_content = "This is a test content."
-    write_file(file_name, file_content)
-    file_content_read = read_file(file_name)
-    assert file_content_read == file_content
+'''Function append_file() in file_io.py'''
+def test_append_to_file():
+    '''file_io.py: append_file() takes an existing filename and input and writes the input to the file.'''
+    write_file('lib/testing/append.txt', 'Hello, World!\n')
+    append_file('lib/testing/append.txt', 'How are you doing today?')
+
